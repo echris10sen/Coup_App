934a3bae4c3cb59b15f542eb5820e1ed6dff2e88
echris10sen <chr15016@byui.edu>
2022-11-08 16:21:29 -0700

    added log

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,10 @@
 function addListeners() {
     document.getElementsByClassName('cyber-button')[0].addEventListener('click', () => {
-        window.location.href="/pages/offline.html"
+        window.location.href="./pages/offline.html"
+        console.log('hello');
     })
     document.getElementsByClassName('cyber-button')[1].addEventListener('click', () => {
-        window.location.href="/pages/instructions.html";
+        window.location.href="./pages/instructions.html";
     });
 }
 
