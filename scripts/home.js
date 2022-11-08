05dc5af42bf75a1150f5f0932fbdf6ca00918146
echris10sen <chr15016@byui.edu>
2022-11-08 16:24:15 -0700

    trying to get it to work

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,6 @@
 function addListeners() {
     document.getElementsByClassName('cyber-button')[0].addEventListener('click', () => {
-        window.location.href="./pages/offline.html"
-        console.log('hello');
+        window.location.href="./pages/offline.html";
     })
     document.getElementsByClassName('cyber-button')[1].addEventListener('click', () => {
         window.location.href="./pages/instructions.html";
