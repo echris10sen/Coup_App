ec0882a737b7b4e536455e2e1adbcc250aeb3bad
echris10sen <chr15016@byui.edu>
2022-11-08 16:07:02 -0700

    testing things with links

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,10 @@
 function addListeners() {
     document.getElementsByClassName('cyber-button')[0].addEventListener('click', () => {
-        window.location.href="../../pages/offline.html"
+        window.location.href="Coup_App/pages/offline.html"
     })
     document.getElementsByClassName('cyber-button')[1].addEventListener('click', () => {
         window.location.href="../../pages/instructions.html";
     });
 }
 
-// addListeners();
+addListeners();
