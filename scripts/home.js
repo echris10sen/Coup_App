1128c5430fe75a386e47a9e2651ce1d6f02e016e
echris10sen <chr15016@byui.edu>
2022-11-08 16:16:21 -0700

    Added links i think correctly

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,9 @@
 function addListeners() {
     document.getElementsByClassName('cyber-button')[0].addEventListener('click', () => {
-        window.location.href="../pages/offline.html"
+        window.location.href="./pages/offline.html"
     })
     document.getElementsByClassName('cyber-button')[1].addEventListener('click', () => {
-        window.location.href="../../pages/instructions.html";
+        window.location.href="./pages/instructions.html";
     });
 }
 
